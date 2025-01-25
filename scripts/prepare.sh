2b9d54d9425f5150b4e13ca98b815321b041048f
Кирилл Груздев <kir1llgruz@yandex.ru>
2025-01-25 14:10:11 +0300

    fix

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -32,6 +32,8 @@ echo "PostgreSQL успешно установлен и запущен."
 echo "Подготовка базы данных..."
 
 # Данные для подключения к базе данных администратора
+DB_HOST="localhost"
+DB_PORT=5432
 DB_ADMIN="postgres"
 DB_NAME="project_sem_1"
 DB_USER="validator"
@@ -39,18 +41,20 @@ DB_PASSWORD="val1dat0r"
 
 # Создание базы данных и пользователя
 echo "Создание базы данных $DB_NAME и пользователя $DB_USER..."
-sudo -u $DB_ADMIN psql postgres <<EOSQL
-CREATE USER $DB_USER WITH ENCRYPTED PASSWORD '$DB_PASSWORD';
-CREATE DATABASE $DB_NAME;
-GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;
-\c $DB_NAME
-CREATE TABLE prices (
-    product_id INTEGER PRIMARY KEY,
-    name TEXT,
-    category TEXT,
-    price NUMERIC,
-    create_date TIMESTAMP
-);
+psql -U "$DB_ADMIN" -h "$DB_HOST" -p "$DB_PORT" <<EOSQL
+DO \$\$ BEGIN
+      IF NOT EXISTS (SELECT FROM pg_catalog.pg_user WHERE usename = 'validator') THEN
+        CREATE USER validator WITH PASSWORD 'val1dat0r';
+      END IF;
+    END \$\$;
+
+    DO \$\$ BEGIN
+      IF NOT EXISTS (SELECT FROM pg_database WHERE datname = '${DBNAME}') THEN
+        CREATE DATABASE ${DBNAME} OWNER validator;
+      END IF;
+    END \$\$;
+
+    GRANT ALL PRIVILEGES ON DATABASE ${DBNAME} TO validator;
 EOSQL
 
 echo "База данных подготовлена."
